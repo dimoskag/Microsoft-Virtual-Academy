4950223397dc6106339e4e1b7057cc6f667c6ae1
dimoskag <dimoskag@users.noreply.github.com>
2016-12-05 17:16:27 +0200

    Dates and Times

---
+++ C-Fundamentals-for-Absolute-Beginners/DatesAndTimes/DatesAndTimes/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatesAndTimes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime myValue = DateTime.Now;
+
+            Console.WriteLine(myValue.ToString());
+            Console.WriteLine(myValue.ToShortDateString());
+            Console.WriteLine(myValue.ToShortTimeString());
+            Console.WriteLine(myValue.ToLongDateString());
+            Console.WriteLine(myValue.ToLongTimeString());
+
+            Console.WriteLine(myValue.AddDays(3).ToLongDateString());
+            Console.WriteLine(myValue.AddHours(3).ToLongTimeString());
+            Console.WriteLine(myValue.AddDays(-3).ToLongDateString());
+
+            Console.WriteLine(myValue.Month);
+
+            DateTime myBirthday = new DateTime(1986, 11, 23);
+            Console.WriteLine(myBirthday.ToShortDateString());
+
+            DateTime myStart = DateTime.Parse("23/11/1986");
+            TimeSpan myAge = DateTime.Now.Subtract(myStart);
+            Console.WriteLine(myAge.TotalDays);
+
+            Console.ReadLine();
+        }
+    }
+}
