559fe0373cac19ff0b9d8e6eb990720be277e2cd
dimoskag <dimoskag@users.noreply.github.com>
2016-12-05 17:29:15 +0200

    Simple Method

---
+++ C-Fundamentals-for-Absolute-Beginners/SimpleMethod/SimpleMethod/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HelloWorld();
+            Console.WriteLine("Please enter your name:");
+            string firstName = Console.ReadLine();
+            Console.WriteLine("Please enter your name:");
+            string lastName = Console.ReadLine();
+            Console.WriteLine("Please enter your city:");
+            string city = Console.ReadLine();
+
+            DisplayResult(ReverseString(firstName), ReverseString(lastName), ReverseString(city));
+            Console.WriteLine();
+            DisplayResult(ReverseString((firstName) + " " + ReverseString(lastName) + ReverseString(city)));
+            Console.ReadLine();
+        }
+
+        private static void HelloWorld()
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        private static string ReverseString(string message)
+        {
+            char[] messageArray = message.ToCharArray();
+            Array.Reverse(messageArray);
+            return string.Concat(messageArray);
+        }
+        public static void DisplayResult(string reversedFirstName, string reversedLastName, string reversedCity)
+        {
+            Console.Write(string.Format("{0} {1} {2}", reversedFirstName, reversedLastName, reversedCity));
+        }
+
+        public static void DisplayResult(string message)
+        {
+            Console.Write(message);
+        }
+
+    }
+}
