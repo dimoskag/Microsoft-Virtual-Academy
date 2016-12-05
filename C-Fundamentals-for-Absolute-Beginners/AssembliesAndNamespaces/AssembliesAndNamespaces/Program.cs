67d2ba229df2e899c7e59452cde1d0e9f4cf5a06
dimoskag <dimoskag@users.noreply.github.com>
2016-12-05 17:10:38 +0200

    Assemblies and Namespaces

---
+++ C-Fundamentals-for-Absolute-Beginners/AssembliesAndNamespaces/AssembliesAndNamespaces/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+
+namespace AssembliesAndNamespaces
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = "Test";
+            File.WriteAllText(@"C:\Test\test\test.txt", text);
+
+            WebClient client = new WebClient();
+            string reply = client.DownloadString("www.microsoft.com");
+            File.WriteAllText(@"C:\Test\test\test2.txt", reply);
+
+            Console.WriteLine(reply);
+            Console.WriteLine("Hello World!");
+
+            Console.ReadLine();
+        }
+    }
+}
