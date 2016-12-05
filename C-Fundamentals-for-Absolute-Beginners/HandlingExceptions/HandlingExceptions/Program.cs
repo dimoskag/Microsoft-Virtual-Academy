094d0bac5de0139ac210d1be51584b6e473134a0
dimoskag <dimoskag@users.noreply.github.com>
2016-12-05 17:26:34 +0200

    Handling Exceptions

---
+++ C-Fundamentals-for-Absolute-Beginners/HandlingExceptions/HandlingExceptions/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace HandlingExceptions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string content = File.ReadAllText(@"C:\Lesson22\Example.txt");
+                Console.WriteLine(content);
+            }
+            catch (FileNotFoundException)
+            {
+                Console.WriteLine("There was a problem!");
+                Console.WriteLine("Make sure the file is named correctly!");
+            }
+            catch (DirectoryNotFoundException)
+            {
+                Console.WriteLine("There was a problem!");
+                Console.WriteLine("Make sure the directory exists");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("There was a problem!");
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                //Code to finalize
+                //Setting objects to null
+                //Closing database connections
+                Console.WriteLine("Closing app now...");
+            }
+            Console.ReadLine();
+        }
+    }
+}
