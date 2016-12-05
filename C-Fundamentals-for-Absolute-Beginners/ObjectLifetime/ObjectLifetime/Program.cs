625f1f157e462d3f5116bb9a0d3d967701878981
dimoskag <dimoskag@users.noreply.github.com>
2016-12-05 17:28:18 +0200

    Object Lifetime

---
+++ C-Fundamentals-for-Absolute-Beginners/ObjectLifetime/ObjectLifetime/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectLifetime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car myCar = new Car();
+
+            Car.myMethod();
+
+            myCar.Make = "Oldsmobile";
+            myCar.Model = "Cutlas Supreme";
+            myCar.Year = 1986;
+            myCar.Color = "silver";
+
+            Car myOtherCar;
+            myOtherCar = myCar;
+
+            Console.WriteLine("{0} {1} {2} {3}", myOtherCar.Make, myOtherCar.Model, myOtherCar.Year, myOtherCar.Color);
+
+            myOtherCar.Model = "98";
+            Console.WriteLine("{0} {1} {2} {3}", myOtherCar.Make, myOtherCar.Model, myOtherCar.Year, myOtherCar.Color);
+
+            myOtherCar = null;
+
+            Car myThirdCar = new Car("ford", "Escape", 2005, "white");
+
+            Console.ReadLine();
+        }
+    }
+
+    class Car
+    {
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public string Color { get; set; }
+        public Car()
+        {
+            Make = "Nissan";
+        }
+
+        public Car(string make,string model, int year, string color)
+        {
+            Make = make;
+            Model = model;
+            Year = year;
+            Color = color;
+        }
+
+        public static void myMethod()
+        {
+            Console.WriteLine("something");
+        }
+    }
+}
