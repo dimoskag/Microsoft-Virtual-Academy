0bbead81fbb57e7a2932d3b8d7a38075e77a4d5b
dimoskag <dimoskagiaras@gmail.com>
2017-10-29 18:48:46 +0200

    Projection Operators

---
+++ .NET/LINQ/Projection Operators/Projection Operators/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace Projection_Operators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            p.SelectSimple1();
+
+            Console.ReadLine();
+        }
+
+        public void SelectSimple1()
+        {
+            int[] numbers = { 5, 4, 1, 3, 9, 8, 6, 7, 2, 0 };
+
+            var numPlusOne = from n in numbers
+                             select n + 1;
+
+            Console.WriteLine("Numbers + 1:");
+            foreach (var i in numPlusOne)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
