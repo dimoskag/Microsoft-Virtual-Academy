36f969d93b2e98432ecb02c3f70dac5d0444c8c0
dimoskag <dimoskagiaras@gmail.com>
2018-04-03 00:01:08 +0300

    Added Bootstrap

--- Angular/Pomodoro-Timer/PomodoroTimer/src/app/app.module.ts
+++ Angular/Pomodoro-Timer/PomodoroTimer/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 
 import { AppComponent } from './app.component';
@@ -12,7 +13,8 @@ import { PomodoroTimerComponent } from './pomodoro-timer/pomodoro-timer.componen
     PomodoroTimerComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent, PomodoroTimerComponent]
