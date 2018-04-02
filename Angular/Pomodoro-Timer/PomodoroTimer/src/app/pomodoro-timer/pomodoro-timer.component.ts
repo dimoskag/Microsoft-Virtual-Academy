f2ddb03b4bdb976034f82e71043699bfb2e3be05
dimoskag <dimoskagiaras@gmail.com>
2018-04-02 19:53:02 +0300

    Added start/resume button

--- Angular/Pomodoro-Timer/PomodoroTimer/src/app/pomodoro-timer/pomodoro-timer.component.ts
+++ Angular/Pomodoro-Timer/PomodoroTimer/src/app/pomodoro-timer/pomodoro-timer.component.ts
@@ -9,6 +9,8 @@ export class PomodoroTimerComponent implements OnInit {
 
   minutes: number;
   seconds: number;
+  isPaused: boolean;
+  buttonLabel: string;
 
   constructor() { 
     this.resetPomodoro();
@@ -16,17 +18,30 @@ export class PomodoroTimerComponent implements OnInit {
   }
 
   tick():void {
-    if(--this.seconds < 0) {
-      this.seconds = 59;
-      if (--this.minutes < 0){
-        this.resetPomodoro();
+    if(!this.isPaused) {
+      this.buttonLabel = 'Pause';
+      if(--this.seconds < 0) {
+        this.seconds = 59;
+        if (--this.minutes < 0){
+          this.resetPomodoro();
+        }
       }
+    }
+  }
+
+  togglePause(): void {
+    this.isPaused = !this.isPaused;
+    // if countdown has started
+    if (this.minutes < 24 || this.seconds < 59) {
+      this.buttonLabel = this.isPaused ? 'Resume' : 'Pause'
     }
   }
 
   resetPomodoro(): void {
     this.minutes = 24;
     this.seconds = 59;
+    this.buttonLabel = 'Start';
+    this.togglePause();
   }
 
   ngOnInit() {
